ad6f61d124a7a6cc92a1c5ce8248518723267a1b
Mac Sta. Maria <mac.stamaria@gmail.com>
2020-07-06 10:41:06 +1000

    Updated codebase.

--- commands.sh
+++ commands.sh
@@ -1,3 +1,14 @@
+#Set up a control node.
 aws cloudformation deploy --template control-node.yaml --stack-name control-node --parameter-overrides KeyName=malago InstanceType=t2.micro SSHLocation=152.32.106.26/32
 
-aws cloudformation delete-stack --stack-name control-node
+#Prepare CloudFormation file.
+region="ap-southeast-2"
+cp -f jenkins.yaml.template jenkins.yaml
+sed -ie "s/__KEYNAME__/makatas/g" jenkins.yaml
+sed -ie "s/__ALLOWED_IP_ADDRESS__/203.87.124.146\/32/g" jenkins.yaml
+
+#Instantiate a Jenkins server.
+aws cloudformation deploy --region $region --template jenkins.yaml --stack-name jenkins
+
+#Delete the stack.
+aws cloudformation delete-stack --stack-name jenkins
